45c2e16e45674821cda86c630a1b57515924a830
AJ Sherrell <amie.sherrell@macys.com>
2022-11-08 15:57:44 -0500

    first commit

---
+++ app/src/main/java/com/ajsherrell/basicstatecodelab/WaterCounter.kt
@@ -0,0 +1,17 @@
+package com.ajsherrell.basicstatecodelab
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    // Water counter counts the number of glasses of water you consume during the day.
+    val count = 0
+    Text(
+        text = "You've had $count glasses.",
+        modifier = modifier.padding(16.dp)
+    )
+}
